c088b5b0db50e99cb208f3f535baec047aa35d9a
dpolve <dawde.din@gmail.com>
2020-05-11 09:02:19 +0200

    3 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { TabellaRegioniComponent } from './tabella-regioni/tabella-regioni.component';
 import { TabellaProvinceComponent } from './tabella-province/tabella-province.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, TabellaRegioniComponent, TabellaProvinceComponent ],
+  declarations: [ AppComponent, TabellaRegioniComponent, TabellaProvinceComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
